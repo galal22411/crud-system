de0ff452a7f0cfb9185ac9a43e185cba04c5f76a
ahmed <ahmed.@email2241.com>
2024-10-04 00:09:23 +0300

    first commit

---
+++ app/Models/ahmedgalal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ahmedgalal extends Model
+{
+    use HasFactory;
+}
